060aaa95c772a8f050063414f455f0cea05da53e
dong.wang <hzwangdong5@corp.netease.com>
2017-12-20 20:49:06 +0800

    Clean dirs

--- src/old_do_more_dojo/core/amd/importing/InvalidDefineException.java
+++
@@ -1,8 +0,0 @@
-package old_do_more_dojo.core.amd.importing;
-
-public class InvalidDefineException extends Exception
-{
-    public InvalidDefineException() {
-        super("The import block was incomplete or invalid");
-    }
-}
--- src/old_do_more_dojo/intellij/actions/DeleteAction.java
+++
@@ -1,9 +0,0 @@
-package old_do_more_dojo.intellij.actions;
-
-public class DeleteAction extends UnusedImportsAction
-{
-    public DeleteAction()
-    {
-        this.deleteMode = true;
-    }
-}
